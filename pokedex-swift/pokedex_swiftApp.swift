9244a5dfc61ba4e7e804782aea57cd2657ad4329
maridaliamart <maridaliamart@gmail.com>
2023-07-20 14:31:36 -0400

    Initial Commit

---
+++ pokedex-swift/pokedex_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_swiftApp.swift
+//  pokedex-swift
+//
+//  Created by Maridalia Martinez on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
