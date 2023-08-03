581c012d20c77b0386bc8393bd993a73b1f634f0
maridaliamart <maridaliamart@gmail.com>
2023-08-03 16:44:57 -0400

    added set up and tear down func

--- pokedex-swiftTests/pokedex_swiftTests.swift
+++ pokedex-swiftTests/pokedex_swiftTests.swift
@@ -11,20 +11,23 @@ import SwiftUI
 @testable import pokedex_swift
 
 class pokedex_swiftTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var testHost: UIHostingController<PokemonView>!
+    
+    override func setUp() {
+       super.setUp()
+        
+        let pokemonViewModel = PokemonViewModel(apiService: MockAPIService(pokemonData: mockPokemonData))
+        let pokemonView = PokemonView(viewModel: pokemonViewModel)
+        let testhost = UIHostingController(rootView: pokemonView)
+        testHost.beginAppearanceTransition(true, animated: false)
+        testHost.endAppearanceTransition()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        testHost = nil
+        super.tearDown()
     }
 
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     let mockPokemonData: [Pokemon] = [
         Pokemon(name: "Pikachu", url: "https://example.com/pikachu.png"),
         Pokemon(name: "Charmander", url: "https://example.com/charmander.png")
@@ -34,9 +37,7 @@ class pokedex_swiftTests: XCTestCase {
         let pokemonViewModel = PokemonViewModel(apiService: MockAPIService(pokemonData: mockPokemonData))
         let pokemonView = PokemonView(viewModel: pokemonViewModel)
         
-        let testhost = UIHostingController(rootView: pokemonView)
-        testHost.beginAppearanceTransition(true, animated: false)
-        testHost.endAppearanceTransition()
+        
         
         XCTAssertNotNil(testHost.view)
         XCTAssertTrue(testHost.view is UITableView)
